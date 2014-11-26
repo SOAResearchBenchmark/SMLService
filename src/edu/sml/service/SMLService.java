42c7cb72ced04c302d507377dcf0e9fff4f6f311
Marios Fokaefs <fokaefs@ualberta.ca>
2014-11-26 14:26:35 -0700

    Fixed to ssrg paths.

--- src/edu/sml/service/SMLService.java
+++ src/edu/sml/service/SMLService.java
@@ -13,12 +13,12 @@ import edu.sml.data.SimpleKMeansResponse;
 public class SMLService {
 	
 	//my paths
-	public static final String LOCAL_FILES_PATH = "WebContent/files/";
-	public static final String SERVER_FILES_PATH = "WebContent/files/";
+	//public static final String LOCAL_FILES_PATH = "WebContent/files/";
+	//public static final String SERVER_FILES_PATH = "WebContent/files/";
 	
 	//ssrg paths
-	//public static final String LOCAL_FILES_PATH = "/var/lib/tomcat7/webapps/SMLService/files/";
-	//public static final String SERVER_FILES_PATH = "http://ssrg18.cs.ualberta.ca:8080/SMLService/files/";
+	public static final String LOCAL_FILES_PATH = "/var/lib/tomcat7/webapps/SMLService/files/";
+	public static final String SERVER_FILES_PATH = "http://ssrg18.cs.ualberta.ca:8080/SMLService/files/";
 	
 	public String getFilesPath() {
 		return LOCAL_FILES_PATH;
